98e0811f8c8ca7b9cd9e795880f636ad0bef855c
Jamie Feeny <feeny.jamie@gmail.com>
2018-08-08 19:23:41 -0700

    Home button set on all view controllers

--- iOSNavigation/ViewController.m
+++ iOSNavigation/ViewController.m
@@ -17,8 +17,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithTitle:@"Home"
+                                                             style:UIBarButtonItemStylePlain
+                                                            target:self
+                                                            action:@selector(showPicker)];
+    [self.navigationItem setRightBarButtonItem:item animated:YES];
 }
 
+- (void) showPicker{
+    [self.navigationController popToRootViewControllerAnimated:YES];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
